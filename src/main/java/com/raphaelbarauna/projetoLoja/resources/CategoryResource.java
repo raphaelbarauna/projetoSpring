2046f7015660b3e994c8108f1ac736227c598901
Raphael Barauna <rafael_baraunaa@hotmail.com>
2019-05-14 21:36:47 +1000

    Class Categoria

--- src/main/java/com/raphaelbarauna/projetoLoja/resources/CategoryResource.java
+++ src/main/java/com/raphaelbarauna/projetoLoja/resources/CategoryResource.java
@@ -1,16 +1,31 @@
 package com.raphaelbarauna.projetoLoja.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import Domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list( ) {
-		return "REST está funcionando!";
+	public List<Category> list( ) {
+		
+		Category cat1 = new Category(1 , "Informatica");
+		Category cat2 = new Category(2 , "Escritorio");
+		
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+ 		
+		return lista;
 	}
 
 }
